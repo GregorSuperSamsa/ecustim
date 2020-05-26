555818a89fc91c3dc7fa7264969bbe5a9c04deac
Georgi Georgiev <georgi8georgiev@gmail.com>
2020-05-27 00:17:19 +0300

    Added initial bluetooth communication class

--- ecustim-gui/Communication/Bluetooth.h
+++ ecustim-gui/Communication/Bluetooth.h
@@ -2,12 +2,31 @@
 #define BLUETOOTH_H
 
 #include <QObject>
+#include <QBluetoothLocalDevice>
+#include <QBluetoothDeviceDiscoveryAgent>
+#include "BluetoothItem.h"
 
 class Bluetooth : public QObject
 {
     Q_OBJECT
 public:
     explicit Bluetooth(QObject *parent = nullptr);
+
+
+
+private:
+    QBluetoothDeviceDiscoveryAgent* agent;
+    QList<QBluetoothAddress> remoteDevices;
+    QBluetoothLocalDevice* device;
+    QString name;
+
+public slots:
+    void onDeviceDiscovered(const QBluetoothDeviceInfo &device);
+    void onDeviceDiscoveryFinished();
+
+signals:
+    //void deviceDiscovered(BluetoothItem item);
+
 
 signals:
 
